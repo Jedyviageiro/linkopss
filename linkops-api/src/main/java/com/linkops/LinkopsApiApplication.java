1d4fb3ea55be48c4ad0a81b13fe9f8983adc9170
Jedy <jedyviageiro16@gmail.com>
2026-08-24 14:00:22 +0200

    chore: initialize LinkOps backend

---
+++ linkops-api/src/main/java/com/linkops/LinkopsApiApplication.java
@@ -0,0 +1,13 @@
+package com.linkops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkopsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkopsApiApplication.class, args);
+	}
+
+}
---
+++ linkops-api/src/test/java/com/linkops/LinkopsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkopsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
